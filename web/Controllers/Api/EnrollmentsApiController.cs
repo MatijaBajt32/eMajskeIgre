888978ab131d9f87ada7c339c867ef42a45c60fc
MatijaBajt32 <matbajt@gmail.com>
2024-01-07 22:36:31 +0100

    Generated Enrollments API

---
+++ web/Controllers/Api/EnrollmentsApiController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Controllers_Api
+{
+    [Route("api/v1/enrollment")]
+    [ApiController]
+    public class EnrollmentsApiController : ControllerBase
+    {
+        private readonly EMIContext _context;
+
+        public EnrollmentsApiController(EMIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EnrollmentsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Enrollment>>> GetEnrollments()
+        {
+          if (_context.Enrollments == null)
+          {
+              return NotFound();
+          }
+            return await _context.Enrollments.ToListAsync();
+        }
+
+        // GET: api/EnrollmentsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Enrollment>> GetEnrollment(int id)
+        {
+          if (_context.Enrollments == null)
+          {
+              return NotFound();
+          }
+            var enrollment = await _context.Enrollments.FindAsync(id);
+
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return enrollment;
+        }
+
+        // PUT: api/EnrollmentsApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEnrollment(int id, Enrollment enrollment)
+        {
+            if (id != enrollment.EnrollmentID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(enrollment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EnrollmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EnrollmentsApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Enrollment>> PostEnrollment(Enrollment enrollment)
+        {
+          if (_context.Enrollments == null)
+          {
+              return Problem("Entity set 'EMIContext.Enrollments'  is null.");
+          }
+            _context.Enrollments.Add(enrollment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEnrollment", new { id = enrollment.EnrollmentID }, enrollment);
+        }
+
+        // DELETE: api/EnrollmentsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEnrollment(int id)
+        {
+            if (_context.Enrollments == null)
+            {
+                return NotFound();
+            }
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Enrollments.Remove(enrollment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EnrollmentExists(int id)
+        {
+            return (_context.Enrollments?.Any(e => e.EnrollmentID == id)).GetValueOrDefault();
+        }
+    }
+}
