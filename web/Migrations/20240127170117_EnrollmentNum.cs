56a2f2dd4e1dd58f70374154b22be527765bf57e
MatijaBajt32 <matbajt@gmail.com>
2024-01-27 18:12:13 +0100

    EnrollmentNumber for Android version in Student

---
+++ web/Migrations/20240127170117_EnrollmentNum.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web.Migrations
+{
+    public partial class EnrollmentNum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EnrollmentNumber",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnrollmentNumber",
+                table: "Student");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
--- web/Models/Student.cs
+++ web/Models/Student.cs
@@ -6,6 +6,8 @@ namespace web.Models
     public class Student
     {
         public int StudentID { get; set; }
+
+        public string? EnrollmentNumber {get; set;}
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public DateTime BirthDate { get; set; }
